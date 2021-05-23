14afad0fd762904c4d58875aa6db655fb60de2c2
Anymorf <anymorf@i.ua>
2021-05-23 19:59:00 +0300

    Версия с ГУИ PySimpleGUI

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-import wx       #wxPython
+import PySimpleGUI as sg
 from dict import *
 myxp = 0        # переменная для вашего Здоровъя
 mydamag = 0     # переменная для вашего урона
@@ -13,125 +13,26 @@ xp = 0          # здоровъе существа
 damag = 0       # Дамаг существа
 loc = 0         # переменная локации
 
-# Next, create an application object.
-# app = wx.App()
 
-# Then a frame.
-# frm = wx.Frame(None, title="Hello World")
-
-"""
-Hello World, but with more meat.
-"""
-
-import wx
-
-class HelloFrame(wx.Frame):
-    """
-    A Frame that says Hello World
-    """
-
-    def __init__(self, *args, **kw):
-        # ensure the parent's __init__ is called
-        super(HelloFrame, self).__init__(*args, **kw)
-
-        # create a panel in the frame
-        pnl = wx.Panel(self)
-
-        # put some text with a larger bold font on it
-        st = wx.StaticText(pnl, label="Hello World!")
-        font = st.GetFont()
-        font.PointSize += 10
-        font = font.Bold()
-        st.SetFont(font)
-
-        # and create a sizer to manage the layout of child widgets
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(st, wx.SizerFlags().Border(wx.TOP|wx.LEFT, 25))
-        pnl.SetSizer(sizer)
-
-        # create a menu bar
-        self.makeMenuBar()
-
-        # and a status bar
-        self.CreateStatusBar()
-        self.SetStatusText("Welcome to wxPython!")
-
-
-    def makeMenuBar(self):
-        """
-        A menu bar is composed of menus, which are composed of menu items.
-        This method builds a set of menus and binds handlers to be called
-        when the menu item is selected.
-        """
-
-        # Make a file menu with Hello and Exit items
-        fileMenu = wx.Menu()
-        # The "\t..." syntax defines an accelerator key that also triggers
-        # the same event
-        helloItem = fileMenu.Append(-1, "&Hello...\tCtrl-H",
-                                    "Help string shown in status bar for this menu item")
-        fileMenu.AppendSeparator()
-        # When using a stock ID we don't need to specify the menu item's
-        # label
-        exitItem = fileMenu.Append(wx.ID_EXIT)
-
-        # Now a help menu for the about item
-        helpMenu = wx.Menu()
-        aboutItem = helpMenu.Append(wx.ID_ABOUT)
-
-        # Make the menu bar and add the two menus to it. The '&' defines
-        # that the next letter is the "mnemonic" for the menu item. On the
-        # platforms that support it those letters are underlined and can be
-        # triggered from the keyboard.
-        menuBar = wx.MenuBar()
-        menuBar.Append(fileMenu, "&File")
-        menuBar.Append(helpMenu, "&Help")
-
-        # Give the menu bar to the frame
-        self.SetMenuBar(menuBar)
-
-        # Finally, associate a handler function with the EVT_MENU event for
-        # each of the menu items. That means that when that menu item is
-        # activated then the associated handler function will be called.
-        self.Bind(wx.EVT_MENU, self.OnHello, helloItem)
-        self.Bind(wx.EVT_MENU, self.OnExit,  exitItem)
-        self.Bind(wx.EVT_MENU, self.OnAbout, aboutItem)
-
-
-    def OnExit(self, event):
-        """Close the frame, terminating the application."""
-        self.Close(True)
-
-
-    def OnHello(self, event):
-        """Say hello to the user."""
-        wx.MessageBox("Hello again from wxPython")
-
-
-    def OnAbout(self, event):
-        """Display an About Dialog"""
-        wx.MessageBox("This is a wxPython Hello World sample",
-                      "About Hello World 2",
-                      wx.OK|wx.ICON_INFORMATION)
-
-
-if __name__ == '__main__':
-    # When this module is run (not imported) then create the app, the
-    # frame, show it, and start the event loop.
-    app = wx.App()
-    frm = HelloFrame(None, title='Hello World 2')
-    frm.Show()
-    app.MainLoop()
 
 
 def start():
-    # Then a frame.
-    # frm = wx.Frame(None, title="Вы проснулись от громкого крика")
-    # Show it.
-    # frm.Show()
+    # Define the window's contents
+    layout = [[sg.Text("What's your name?")],  # Part 2 - The Layout
+              [sg.Input()],
+              [sg.Button('Ok')]]
 
-    # Start the event loop.
-    # app.MainLoop()
+    # Create the window
+    window = sg.Window('Window Title', layout)  # Part 3 - Window Defintion
+
+    # Display and interact with the Window
+    event, values = window.read()  # Part 4 - Event loop or Window.read call
+
+    # Do something with the information gathered
+    print('Hello', values[0], "! Thanks for trying PySimpleGUI")
+
+    # Finish up by removing from the screen
+    window.close()  # Part 5 - Close the Window
     print('Вы проснулись от громкого крика. Убрав с лица пальмовый лист, вы осмотрелись вокруг.')
     print('Вы увидели лишь тропические заросли, где-то в далеке слышался шум волн.')
     print('Чтобы выйти напишите: выход.')
@@ -443,7 +344,7 @@ def fight():
     global mydamag
     global xp
     global damag
-    playerAtak = 0
+    playerAtak=0
     print(f"Атаковать? {animal} с ударом {mydamag}")
     print(f"Бежать? от {animal} то он ударит вас 2 раза с уроном {damag}")
     print("Твой выбор Атака/Бежать")
@@ -484,10 +385,11 @@ def fight():
                 print(f"Вы бьете животное с уроном {mydamag}")
                 print(f"Вас бьют с уроном {damag}")
                 print(f"Увас здоровья {myxp} Здоровье животного {xp}")
-                if myxp < 1:
+                if myxp <= 1:
                     print("Вы умерли")
+                    input()
                     exit()
-                elif xp < 0:
+                elif xp <= 0:
                     print("Вы выйграли")
                     break
                 else:
